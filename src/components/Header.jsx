d9b0effcfc90b5308ae5dfc17613fb2c1a570de1
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-03-17 10:55:37 -0500

    implement sliding nav logic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,25 @@
-import styled from 'styled-components'
-import Logo from '../assets/logo.png'
-import Nav from './Nav'
-import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import styled from 'styled-components';
+import Logo from '../assets/logo.png';
+import Nav from './Nav';
+import { Link } from 'react-router-dom';
 
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <Wrapper>
       <Link to="/"><img src={Logo} alt="logo" /></Link>
-      <Nav />
+      <Hamburger onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </Hamburger>
+      <Nav isMenuOpen={isMenuOpen} />
     </Wrapper>
   )
 }
@@ -17,15 +29,30 @@ const Wrapper = styled.header`
   height: 100px;
   align-items: center;
   background-color: var(--accent-white);
-  
-  padding: 0 0 0 30px;
+  padding: 0 30px;
+  justify-content: space-between;
 
   img {
     max-width: 150px;
   }
+`;
 
-  background-color: var(--accent-white);
+const Hamburger = styled.div`
+  display: none;
+  flex-direction: column;
+  gap: 5px;
+  cursor: pointer;
 
-`
+  span {
+    display: block;
+    width: 25px;
+    height: 3px;
+    background-color: black;
+  }
 
-export default Header
+  @media (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+export default Header;
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,39 +1,54 @@
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-function Nav() {
+function Nav({ isMenuOpen }) {
   return (
-    <Wrapper>
-        <Link to="/services">SERVICES</Link>
-        <Link to="/about">ABOUT</Link>
-        <Link to="/rates">RATES</Link>
-        <Link to="/employment">EMPLOYMENT</Link>
-        <Link to="/contact">CONTACT</Link>   
+    <Wrapper isMenuOpen={isMenuOpen}>
+      <Link to="/services">SERVICES</Link>
+      <Link to="/about">ABOUT</Link>
+      <Link to="/rates">RATES</Link>
+      <Link to="/employment">EMPLOYMENT</Link>
+      <Link to="/contact">CONTACT</Link>
     </Wrapper>
   )
 }
 
-const Wrapper = styled.nav` 
-    flex: 1;
-    display: flex; 
-    gap: 20px;
-    justify-content: space-around;
-    
+const Wrapper = styled.nav`
+  flex: 1;
+  display: flex;
+  gap: 20px;
+  justify-content: space-around;
 
-    a{
-        font-size: 1.2rem;
-        font-weight: 700;
-        text-decoration: none;
-        color: black;
+  a {
+    font-size: 1.2rem;
+    font-weight: 700;
+    text-decoration: none;
+    color: black;
+  }
+
+  a:hover {
+    color: var(--hover-blue);
+  }
+
+  a:active {
+    color: var(--active-blue);
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    position: absolute;
+    top: 100px;
+    right: 0;
+    background-color: var(--accent-white);
+    width: 200px;
+    transform: ${({ isMenuOpen }) => isMenuOpen ? 'translateX(0)' : 'translateX(100%)'};
+    transition: transform 0.3s ease-in-out;
+
+    a {
+      text-align: center;
+      padding: 10px;
     }
+  }
+`;
 
-    a:hover{
-        color: var(--hover-blue)
-    }
-
-    a:active{
-        color: var(--active-blue)
-    }
-
-`
-export default Nav
+export default Nav;
