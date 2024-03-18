a8278179c85d2976b7d16eceda23770587463014
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-03-17 20:51:23 -0500

    Overlap text on image

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styled from "styled-components";
 import GoogleReviewsLogo from "../assets/googlereviews.png";
 import {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import background2 from "../assets/background2.webm";
 import samsara from "../assets/samsara_home.mp4";
 import { Link } from "react-router-dom";
 
@@ -11,7 +11,13 @@ const Home = () => {
             TAA Logistics, Inc.
           </h1>
           <p className="mt-4 text-gray-600">
-            TAA Logistics, Inc. is a rapidly expanding transportation company based in the Midwest, with a team of skilled drivers experienced in intermodal transport operating our fleet of modern power units. Our fleet consists of over 30 fuel-efficient trucks, all equipped with the latest electronic logging devices (ELDs) and tracking software. Additionally, our team of certified mechanics ensures the ongoing maintenance of our trucks and company-owned chassis.
+            TAA Logistics, Inc. is a rapidly expanding transportation company
+            based in the Midwest, with a team of skilled drivers experienced in
+            intermodal transport operating our fleet of modern power units. Our
+            fleet consists of over 30 fuel-efficient trucks, all equipped with
+            the latest electronic logging devices (ELDs) and tracking software.
+            Additionally, our team of certified mechanics ensures the ongoing
+            maintenance of our trucks and company-owned chassis.
           </p>
           <div className="mt-6">
             <Link
@@ -24,29 +30,40 @@ const Home = () => {
               to="/employment"
               className="bg-gray-500 text-white font-bold py-2 px-4 rounded hover:bg-gray-700"
             >
-              
               Apply Now
             </Link>
           </div>
         </div>
         <div className="md:w-1/2 mt-8 md:mt-0">
-
-<video
+          <video
             src={samsara}
             className="rounded-lg shadow-lg"
             autoPlay
-            controls={false}
             loop
-            />
-
+            muted
+          />
         </div>
       </div>
-
       <div className="mt-12">
-        <h2 className="text-3xl font-bold text-gray-800">About Us</h2>
-        <p className="mt-4 text-gray-600">
-          As a family-owned and operated transportation business, we are deeply committed to the future of our industry. Since our inception in 1995, we have expanded our operations across two countries. With a collective experience of over 5 million miles, we have established ourselves as leaders in the transportation sector. TAA Logistics is proudly driven by its three original drivers, who are also the owners of the company.
-        </p>
+        <div className="relative">
+          <video
+            src={background2}
+            className="rounded-lg shadow-lg w-full h-96 object-cover"
+            autoPlay
+            controls={false}
+            loop
+          />
+          <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white w-3/4">
+            <h2 className="text-3xl font-bold">About Us</h2>
+            <p className="mt-4">
+              As a family-owned transportation company, we're committed to our
+              industry's future. Since 1995, we've operated in two countries and
+              amassed over 5 million miles of experience, making us leaders in
+              the transport sector. Our founders, who are also our original
+              drivers, own TAA Logistics.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
