23e2e1e2f62a613dd9bbc76647be53221e9fa1be
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-03-17 21:03:52 -0500

    add new font

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
       <div className="flex flex-col md:flex-row items-center gap-8">
         <div className="md:w-1/2">
           <h1 className="text-4xl font-bold text-gray-800">
-            TAA Logistics, Inc.
+            TAA Logistics Inc.
           </h1>
           <p className="mt-4 text-gray-600">
             TAA Logistics, Inc. is a rapidly expanding transportation company
@@ -56,8 +56,8 @@ const Home = () => {
           <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white w-3/4">
             <h2 className="text-3xl font-bold">About Us</h2>
             <p className="mt-4">
-              As a family-owned transportation company, we're committed to our
-              industry's future. Since 1995, we've operated in two countries and
+              As a family-owned transportation company, we&apos;re committed to our
+              industry&apos;s future. Since 1995, we&apos;ve operated in two countries and
               amassed over 5 million miles of experience, making us leaders in
               the transport sector. Our founders, who are also our original
               drivers, own TAA Logistics.
