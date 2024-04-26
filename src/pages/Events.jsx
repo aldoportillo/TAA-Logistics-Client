e04cc7823244d106463444af50deecd82d55bd28
aldoportillo <94483081+aldoportillo@users.noreply.github.com>
2024-04-25 19:27:32 -0500

    animate gallery

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -2,20 +2,22 @@ import PropTypes from 'prop-types';
 
 function Events({ events }) {
     return (
-        <div className="p-5 flex-grow-1">
-            {events.map((event) => (
-                <div key={event.id} className="mb-10">
-                    <h2 className="text-2xl text-gray-800 font-bold mb-2">{event.name}</h2>
-                    <p className="text-gray-500 mb-4">{event.description}</p>
-                    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
-                        {event.images.map((image) => (
+        <div className="p-8 flex flex-col-reverse flex-grow">
+            {events && events.map((event) => (
+                <div key={event.id} className="mb-10 bg-white shadow-lg rounded-lg overflow-hidden">
+                    <h2 className="text-3xl text-gray-800 font-bold px-6 pt-6">{event.name}</h2>
+                    <p className="text-gray-600 px-6 mb-4">{event.description}</p>
+                    <div className="overflow-hidden relative">
+                        <div className="whitespace-nowrap animate-marquee">
+                            {event.images.concat(event.images).map((image, index) => (  // Duplicate the array of images
                                 <img
-                                    key={image.id}
+                                    key={`${image.id}-${index}`}  // Unique key for duplicated items
                                     src={image.url}
                                     alt={`Image from ${event.name}`}
-                                    className="w-full h-auto object-cover rounded transform hover:scale-110 transition-transform duration-300"
+                                    className="inline-block w-auto h-auto max-h-48 object-cover mr-4"
                                 />
-                        ))}
+                            ))}
+                        </div>
                     </div>
                 </div>
             ))}
